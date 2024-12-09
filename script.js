31e9b20b7ed75700371637724ed42fa068b9c175
MartinGacev <martingacev@gmail.com>
2024-12-09 14:44:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function typeAnswer(type) {
+    console.log( typeof type) 
+  }
+ 
+ typeAnswer (59)             
+typeAnswer ("Hell my name is Martins")
+  typeAnswer(true)         
+  typeAnswer([])      
+  typeAnswer(undefined)     
+
+let myAge = 20
+ dogAge = myAge * 7 
+ console.log(dogAge)
+
+ let dogAgeConvert = 140
+ myAgeConvert = dogAge / 7
+ console.log(myAgeConvert)
